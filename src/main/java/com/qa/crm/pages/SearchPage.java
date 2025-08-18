e7f027adc0b45831eee5905cfa0be450cab893e6
Satrajit Nath <satrajitn25@gmail.com>
2025-08-19 01:09:04 +0530

    java

--- src/main/java/com/qa/crm/pages/SearchPage.java
+++ src/main/java/com/qa/crm/pages/SearchPage.java
@@ -1,9 +1,13 @@
 package com.qa.crm.pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SearchPage {
 	
 	public void search() {
-		System.out.println("print");
+		WebDriver driver = new ChromeDriver();
+		System.out.println("Search");
 	}
 
 }
