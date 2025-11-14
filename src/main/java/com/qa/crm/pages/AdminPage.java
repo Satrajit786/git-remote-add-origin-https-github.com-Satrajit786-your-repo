f1b3d8739d7f9915e47c3d58d75d677149b43c7a
Satrajit Nath <satrajit748@gmail.com>
2025-11-14 20:20:38 +0530

    merge

--- src/main/java/com/qa/crm/pages/AdminPage.java
+++ src/main/java/com/qa/crm/pages/AdminPage.java
@@ -1,7 +1,13 @@
 package com.qa.crm.pages;
 
+import org.openqa.selenium.WebDriver;
+
 public class AdminPage {
 	
+	public AdminPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+	}
+
 	public void testgit() {
 		
 		System.out.println("Joy EastBengal");
--- src/main/java/com/qa/crm/pages/HomePage.java
+++ src/main/java/com/qa/crm/pages/HomePage.java
@@ -8,57 +8,33 @@ import org.openqa.selenium.support.PageFactory;
 import com.crm.qa.bae.TestBase;
 
 public class HomePage extends TestBase {
-	
-	@FindBy(xpath = "//p[contains(text(),'Luciano Sobrinho')]")
-	
-	WebElement username;
-	
-	@FindBy(xpath ="//span[normalize-space()='Admin']")
-	
-	WebElement adminlink;
-	
-	@FindBy(xpath = "//span[normalize-space()='Leave']")
-	
-	WebElement leavelink;
-	
-	// after declaring xpath we need to initialize constructor to initialixe homepage
-	
-	
-	public HomePage(WebDriver driver) {
-		
-		this.driver = driver ;
-		
-		PageFactory.initElements(driver, this);
-	}
-	
-	public String verifyHomePageTittle() {
-		 return driver.getTitle();
-	}
-	
-  public boolean verifyCorrectUserName() {
-	   return username.isDisplayed();
-  }
-	
-	public AdminPage clickonAdminLink() {
-		
-		adminlink.click();
-		
-		return new AdminPage();
-		
-		
-	}
-	
-	public LeavePage clickonLeaveLink() {
-		
-	leavelink.click();
-		
-		return new LeavePage();
-	
-	}
-	
-	
-	
-	
-	
+
+    @FindBy(xpath = "//p[contains(text(),'Luciano Sobrinho')]")
+    WebElement username;
+
+    @FindBy(xpath = "//span[normalize-space()='Admin']")
+    WebElement adminlink;
+
+    @FindBy(xpath = "//span[normalize-space()='Leave']")
+    WebElement leavelink;
+
+    // Constructor to initialize page elements
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public String verifyHomePageTittle() {
+        return driver.getTitle();
+    }
+
+    public boolean verifyCorrectUserName() {
+        return username.isDisplayed();
+    }
+
+    public AdminPage clickonAdminLink() {
+        adminlink.click();
+        return new AdminPage(driver); // pass driver to next page
+    }
 
 }
