a72076b8d19e0fcde69ad181f51e87f43f92228f
Satrajit Nath <satrajit748@gmail.com>
2026-01-07 13:48:13 +0530

    added/deleted/modified

---
+++ src/main/java/com/qa/crm/pages/GitPrac.java
@@ -0,0 +1,25 @@
+package com.qa.crm.pages;
+
+public class GitPrac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("practicing git");
+
+		String b1 ="MSS";
+		
+		String B2 = "SN25";
+		
+		String s = "BSS";
+		
+		System.out.println(b1.charAt(2));
+		
+		System.out.println(b1.equals(B2));
+		
+		System.out.println(b1.contains(B2));
+		
+		System.out.println(b1.compareTo(s));
+	}
+
+}
